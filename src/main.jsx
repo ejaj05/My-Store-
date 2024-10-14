893df3b4453c6a705f5bae89b1eab0afc097ff70
ejaj05 <alamejaj288@gmail.com>
2024-10-14 19:46:39 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import Context from "./Utils/Context.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
   <Context>
     <BrowserRouter>
         <App />
+      <ToastContainer/>
     </BrowserRouter>
   </Context>
 );
